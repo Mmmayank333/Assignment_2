291ca67d78207fbb60cd8b9f5a9e86643896a0fb
Mmmayank333 <57066239+Mmmayank333@users.noreply.github.com>
2019-11-13 02:25:52 +0530

    Add files via upload

--- Q2_MT2018512.s
+++ Q2_MT2018512.s
@@ -1,28 +1,61 @@
+;tan(x)    
 	AREA     appcode, CODE, READONLY
-    EXPORT __main
-    ENTRY 
-
+     EXPORT __main
+     ENTRY 
+__main  FUNCTION 
+		VLDR.F32 S0, =2.617980 ;Storing the value of x in S0
 		
-			MOV R1, #0x4;
-			MOV R2, #0x2;
-			MOV R3, #0x5;
-			cmp R1, R2
-			ble false1
-			cmp R1, R3
-			ble false2
-			MOV R8, #0x1;
-			b stop
+		;Calculating sin(x)
+		MOV R0,#1
+		MOV R1,#2 ;Using R0,R1,R2 for calculating the factorial for every term
+		MOV R2,#3
+		MOV R3,#0
 		
-false1		cmp R2,R3 
-			ble false3
-			MOV R8, #0x2;
-			b stop
+		VLDR.F32 S1, =1
+		VMUL.F32 S2,S0,S0
+		
+loop_sin	VDIV.F32 S0,S0,S1
+		VADD.F32 S3,S3,S0
+		VMUL.F32 S0,S2,S0 ;Calculating each term of sin series
+		MUL R0,R1,R2 ;Calculating factorial
+		ADD R1,R1,#2 ;Adding 2 for calculating next term factorial
+		ADD R2,R2,#2 ;Adding 2 for calculating next term factorial
+		VNEG.F32 S0,S0 ;Negating the alternate term of series
+		VMOV.F32 S1, R0 ;Moving the value of register R0 to floating register S1
+		VCVT.F32.U32 S1,S1 ;Converting the value of S1 from exponential form to floating point number
+		ADD R3,R3,#1 ;Incrementing the counter
+		CMP R3,#10 ;Comparing the counter value
+		BNE loop_sin
 
-false2		MOV R8, #0x3;
-			b stop
-false3		MOV R8, #0x3;			
-			b stop
 
-stop    B stop ; stop program
-     ENDFUNC
-     END
+
+		;Calculating cos(x)
+		;Similar steps as defined in above sin series
+		MOV R4, #3;
+		MOV R5, #4;
+		VLDR.F32 S4, =1;
+		VMOV.F32 S9, S2;
+		VNEG.F32 S9,S9;
+		VMOV.F32 S5,S9;
+		VLDR.F32 S1, =2;
+		MOV R3,#0;
+		
+loop_cos	VDIV.F32 S5,S5,S1;
+		VADD.F32 S4,S4,S5;
+		VMUL.F32 S5,S5,S2;
+		MUL R0,R4,R5;
+		VMOV.F32 S1, R0;
+		VCVT.F32.U32 S1,S1;
+		VNEG.F32 S5,S5;
+		ADD R4,R4,#2;
+		ADD R5,R5,#2;
+		ADD R3,R3,#1;
+		CMP R3,#10;
+		BNE loop_cos
+		
+		VDIV.F32 S10,S3,S4;
+
+stop    B stop ; stop program?
+	ENDFUNC
+	END 
+		
